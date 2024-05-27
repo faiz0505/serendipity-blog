b3854f1f53190085965791fe834a5f56561b33b1
Faiz Al <faizali786313@gmail.com>
2024-05-27 06:29:25 +0530

    middleware updated

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ["/", "/about", "/contact", "post/:id", "query/:slug"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
