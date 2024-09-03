2bde35f2b98c15e1c74b189263cd701931c901f1
Faiz Al <faizali786313@gmail.com>
2024-09-03 18:04:49 +0530

    added google console link

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 export const metadata = {
-  metadataBase: "https://serendipityblog.vercel.app",
-  title: "Serendipty",
+  metadataBase: new URL("https://serendipityblog.vercel.app"),
+  title: "Serendipty Blog",
   description:
     "Discover a world of inspiration with our blog platform. Engage with diverse content, connect with like-minded readers, and share your stories. Explore topics ranging from lifestyle and tech to travel and wellness. Join our community today and start your journey of endless inspiration and insightful conversations",
-  keywords:
+  keywords: [
     "serendipity blog, blogs, technology, tech insights, tech articles, lifestyle blogs",
+  ],
+  verification: {
+    google: "r1wALZ2zHe3rrnLtmtrUOyLQqITUJH9wWCCSYeIuiAE",
+  },
 };
 export default function RootLayout({ children }) {
   return (
